4c0ada17ff1b2741d041f7095c2fea10fb11ad67
kamkash <mrkamkash@gmail.com>
2025-04-13 17:22:06 -0600

    test cases

--- math-parser/tests/general_tests.rs
+++ math-parser/tests/general_tests.rs
@@ -54,29 +54,29 @@ mod gen {
 
             fn visit_equation(&mut self, ctx: &EquationContext<'input>) -> Self::Return {
                 dbg!(ctx.get_text());
-                for child in ctx.get_children() {
-                    if child.get_child_count() > 0 {
-                        for grand_child in child.get_children() {
-                            if grand_child.get_child_count() > 0 {
-                                for grand_grand_child in grand_child.get_children() {
-                                    if grand_grand_child.get_child_count() > 0 {
-                                        for grand_grand_grand_child in
-                                            grand_grand_child.get_children()
-                                        {
-                                            dbg!(grand_grand_grand_child.get_text());
-                                        }
-                                    } else {
-                                        dbg!(grand_grand_child.get_text());
-                                    }
-                                }
-                            } else {
-                                dbg!(grand_child.get_text());
-                            }
-                        }
-                    } else {
-                        dbg!(child.get_text());
-                    }
-                }
+                // for child in ctx.get_children() {
+                //     if child.get_child_count() > 0 {
+                //         for grand_child in child.get_children() {
+                //             if grand_child.get_child_count() > 0 {
+                //                 for grand_grand_child in grand_child.get_children() {
+                //                     if grand_grand_child.get_child_count() > 0 {
+                //                         for grand_grand_grand_child in
+                //                             grand_grand_child.get_children()
+                //                         {
+                //                             dbg!(grand_grand_grand_child.get_text());
+                //                         }
+                //                     } else {
+                //                         dbg!(grand_grand_child.get_text());
+                //                     }
+                //                 }
+                //             } else {
+                //                 dbg!(grand_child.get_text());
+                //             }
+                //         }
+                //     } else {
+                //         dbg!(child.get_text());
+                //     }
+                // }
                 self.visit_children(ctx)
             }
 
@@ -127,7 +127,8 @@ mod gen {
             }
         }
 
-        let input = r#"a=cos(y)^2 + sin(x)^2 , 
+        let input = r#"10 + 21 = ans,
+                            a=cos(y)^2 + sin(x)^2 , 
                             b=5000.0, 
                             c=2,
                             d=$1000.00,
